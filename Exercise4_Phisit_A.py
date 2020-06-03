5b5e152aeb823f4ae54e610532158e2ddb385305
NewbyDev <phisit_tiang@hotmail.com>
2020-06-03 19:46:28 +0700

    Exercise4

---
+++ Exercise4_Phisit_A.py
@@ -0,0 +1,12 @@
+F = 89.0
+G = 90.0
+In = 79.5
+Com = 89.9
+
+print("------------- Your Score --------------")
+print("Foundation English               :",  F)
+print("General Business                 :",  G)
+print("Introduction to Computer Systems :", In)
+print("Computer Programming             :",Com)
+print("------------- Complete ----------------")
+
