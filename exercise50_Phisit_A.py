4b1829da47c64b27d28c643bfc0a26d5bc486b62
NewbyDev <phisit_tiang@hotmail.com>
2020-06-14 20:06:29 +0700

    Function Homework
    +,-,*,/

---
+++ exercise50_Phisit_A.py
@@ -0,0 +1,13 @@
+def sumNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def devisionNumber(x,y):
+    print(x/y)
+
+sumNumber(10,2)
+minusNumber(100,20)
+multiplyNumber(1000,200)
+devisionNumber(10000,2000)
